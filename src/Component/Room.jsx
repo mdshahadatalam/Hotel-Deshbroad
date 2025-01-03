81fa9c1b92cea33ae491cc7f360f627e3319a1bc
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-01-02 21:09:38 -0800

    added

--- src/Component/Room.jsx
+++ src/Component/Room.jsx
@@ -3,6 +3,11 @@ import axios from 'axios'
 import { toast, ToastContainer } from 'react-toastify';
 
 export const Room = () => {
+
+ const [modal,setModal] = useState(false)
+
+
+
   const [img,setImg] = useState("")
   const [price,setPrice] = useState("")
   const [subHeading, setSubHeading] = useState("")
@@ -24,6 +29,11 @@ export const Room = () => {
   }
   const handleFile =(e)=>{
     setImg(e.target.files[0])
+  }
+
+
+  const handleOpen =()=>{
+    setModal(true)
   }
   
 
@@ -235,6 +245,96 @@ export const Room = () => {
         
     </tbody>
 </table>
+
+
+{/* modal  */}
+
+
+
+
+
+  {
+    modal === true ?  <div className='modalP'>
+    <div className="p-6 bg-white rounded-lg shadow-lg w-full max-w-lg mx-auto">
+        <p className='text-center text-xl text-slate-600'>Add your room here</p>
+       <div className="mb-2">
+       <label className="block text-sm font-semibold text-gray-700 mb-2" htmlFor="file-upload">
+         Upload File
+       </label>
+       <input
+         // onChange={handleFile}
+         className="py-2 px-4 w-full border border-[#E0E0E0] rounded-lg shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+         type="file"
+         id="file-upload"
+       />
+     </div>
+    
+     <div className="mb-2">
+       <label className="block text-sm font-semibold text-gray-700 mb-2" >
+         Price
+       </label>
+       <input
+         // value={price}
+         // onChange={handlePrice}
+         className="w-full h-12 px-4 border border-[#E0E0E0] rounded-lg shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+         type="text"
+         id="menu-name"
+         placeholder="Price"
+       />
+     </div>
+    
+     <div className="mb-2">
+       <label className="block text-sm font-semibold text-gray-700 mb-2" >
+       Sub Heading
+       </label>
+       <input
+         //  value={subHeading}
+         //  onChange={handleSubHead}
+         className="w-full h-12 px-4 border border-[#E0E0E0] rounded-lg shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+         type="text"
+         id="menu-name"
+         placeholder="Sub heading"
+       />
+     </div>
+    
+     <div className="mb-2">
+       <label className="block text-sm font-semibold text-gray-700 mb-2" >
+         Heading
+       </label>
+       <input
+         value={head}
+         onChange={handleHead}
+         className="w-full h-12 px-4 border border-[#E0E0E0] rounded-lg shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+         type="text"
+         id="menu-name"
+         placeholder="Head"
+       />
+     </div>
+    
+     <div className="mb-2 flex items-center">
+       <input
+       //  checked={showImg}
+       //  onChange={handleImgShow}
+        className="my-2 h-4 w-4 border-[#E0E0E0] rounded-sm focus:ring-2 focus:ring-indigo-500" type="checkbox" />
+       <label className="text-sm ps-2 text-gray-700" htmlFor="show-button">Show Image</label>
+     </div>
+    
+    
+     <div className="mt-3 text-center">
+       <button
+         //  onClick={handleSubmit}
+         className="px-6 py-2 bg-indigo-600 text-white font-semibold text-sm rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+       >
+         Submit
+       </button>
+     </div>
+    </div>
+    </div>
+      : null}
+ 
+ 
+<button className='btn btn-success' onClick={handleOpen}>open</button>
+ 
 
 <ToastContainer />
 
